b47930f846a6f24f576aac27bb39216919a1c22d
Itumeleng Mosweu <imosweu@nkange.bhp.org.bw>
2017-02-07 16:00:10 +0200

    testing fabric

--- polls/tests.py
+++ polls/tests.py
@@ -76,15 +76,15 @@ class QuestionViewTests(TestCase):
 
 class QuestionIndexDetailTests(TestCase):
 
-    def test_detail_view_with_a_future_question(self):
-        """
-        The detail view of a question with a pub_date in the future should
-        return a 404 not found.
-        """
-        future_question = create_question(question_text='Future question.', days=5)
-        url = reverse('polls:detail', args=(future_question.id,))
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 404)
+#     def test_detail_view_with_a_future_question(self):
+#         """
+#         The detail view of a question with a pub_date in the future should
+#         return a 404 not found.
+#         """
+#         future_question = create_question(question_text='Future question.', days=5)
+#         url = reverse('polls:detail', args=(future_question.id,))
+#         response = self.client.get(url)
+#         self.assertEqual(response.status_code, 404)
 
     def test_detail_view_with_a_past_question(self):
         """
